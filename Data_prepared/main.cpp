cedfca375fee30708180ae5ffea2b76b1421b48c
Shiming Chen <g_shmchen@163.com>
2018-08-04 14:55:54 +0800

    Update main.cpp

--- Data_prepared/main.cpp
+++ Data_prepared/main.cpp
@@ -3,7 +3,6 @@
 int main() {
     LineSegmentation line_segmentation("D:\\Writer identification\\data\\HistoricalWI_Query_binary_origin\\0005-IMG_MAX_998376.jpg");
     vector<cv::Mat> lines = line_segmentation.segment();
-    // ToDo @Samir55 Remove.
     int idx = 0;
     for(auto m : lines) {
         imwrite("Line " + to_string(idx++) + ".jpg", m);
